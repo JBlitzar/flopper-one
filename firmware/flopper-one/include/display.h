edfbe5a2b42d8db232e278b8768d5c7a1b33650a
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2026-03-03 22:22:31 -0800

    Update display.h

--- firmware/flopper-one/include/display.h
+++ firmware/flopper-one/include/display.h
@@ -1,6 +1,9 @@
 #pragma once
-
+#include <tft_espi_setup.h>
 #include "flopper_pins.h"
+#include <TFT_eSPI.h>
+#include <SPI.h>
+#include <string>
 
 namespace flopper
 {
@@ -14,8 +17,45 @@ namespace flopper
             return instance;
         }
 
+        void clear(uint32_t color)
+        {
+            tft.fillScreen(color);
+        }
+
+        void draw_text(int x, int y, std::string &str, uint32_t color, int size)
+        {
+            tft.setTextColor(color, TFT_BLACK);
+            tft.setTextSize(size);
+            tft.drawString(str.c_str(), x, y);
+        }
+
+        void draw_text(int x, int y, std::string &str, uint32_t color, int size, uint32_t highlight)
+        {
+            tft.setTextColor(color, highlight, true);
+            tft.setTextSize(size);
+            tft.drawString(str.c_str(), x, y);
+        }
+
+        void fill_rect(int x, int y, int w, int h, uint32_t color)
+        {
+            tft.fillRect(x, y, w, h, color);
+        }
+
+        void fill_rect(int x, int y, int w, int h, uint32_t color, int r)
+        {
+            tft.fillRoundRect(x, y, w, h, r, color);
+        }
+
     private:
-        Display() = default;
+        TFT_eSPI tft;
+        Display()
+        {
+
+            tft = TFT_eSPI();
+            tft.init();
+            tft.setRotation(0);
+            tft.fillScreen(TFT_BLACK);
+        };
 
         // copied + edited from google AI overview lol
 
