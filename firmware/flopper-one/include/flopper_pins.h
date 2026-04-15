4d3cb65c06c242ecea1ccc7282c89353a6874e1d
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2026-04-14 19:54:07 -0700

    correct pins

--- firmware/flopper-one/include/flopper_pins.h
+++ firmware/flopper-one/include/flopper_pins.h
@@ -9,8 +9,8 @@ namespace flopper::pins
     static constexpr uint8_t TSOP_OUT = 23; // TSOP IR receiver out
     static constexpr uint8_t DPAD_UP = 14;  // normal buttons
     static constexpr uint8_t DPAD_LEFT = 27;
-    static constexpr uint8_t DPAD_RIGHT = 12;
-    static constexpr uint8_t DPAD_DOWN = 26;
+    static constexpr uint8_t DPAD_RIGHT = 26;
+    static constexpr uint8_t DPAD_DOWN = 12;
     static constexpr uint8_t DPAD_CENTER = 25;
 
     static constexpr uint8_t PN532_SDA = 17; // pn532 nfc/rfid thing
