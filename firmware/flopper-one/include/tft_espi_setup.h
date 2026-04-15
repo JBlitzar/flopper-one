7f0f28aab3477fa9d801202694b26c7a4a15fb87
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2026-04-14 19:15:33 -0700

    Revert "Update tft_espi_setup.h"
    This reverts commit 7c41c03ec66cde922960aec06c67519122e50c42.

--- firmware/flopper-one/include/tft_espi_setup.h
+++ firmware/flopper-one/include/tft_espi_setup.h
@@ -3,8 +3,6 @@
 // TFT_eSPI setup (forced-in via platformio.ini build_flags)
 // Display: ST7789, SPI, 240x240
 
-#define USE_HSPI_PORT
-
 #define ST7789_DRIVER
 
 // Fonts
@@ -17,7 +15,7 @@
 // #define LOAD_FONT7
 // #define LOAD_FONT8
 
-#define TFT_WIDTH 240
+#define TFT_WIDTH  240
 #define TFT_HEIGHT 240
 
 // ESP32 SPI wiring
@@ -26,8 +24,10 @@
 #define TFT_MISO -1
 
 // Control pins
-#define TFT_DC 32
-#define TFT_CS -1
+#define TFT_DC  32
+#define TFT_CS  -1
 #define TFT_RST 33
 
-#define SPI_FREQUENCY 0
+#define TFT_INVERSION_ON
+
+#define SPI_FREQUENCY 4000000
